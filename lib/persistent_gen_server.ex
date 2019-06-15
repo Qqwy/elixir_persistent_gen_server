318ba69419a3bc3c3679e014318895bbf70f5d79
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-06-16 00:34:12 +0200

    delegate Terminate properly.

--- lib/persistent_gen_server.ex
+++ lib/persistent_gen_server.ex
@@ -41,7 +41,7 @@ defmodule PersistentGenServer do
       '0' means: indefinite.
       TODO maybe change to`:infinity`?
       """
-      field :petrification_timeout, :integer, default: 0
+      field :petrification_timeout, :integer, default: 1
 
       @doc """
       The atom name (or module name) of the Dynamic Supervisor
@@ -67,7 +67,7 @@ defmodule PersistentGenServer do
     end
   end
 
-  defstruct [:module, :init_args, :internal_state, config: Config.load()]
+  defstruct [:module, :init_args, :internal_state, config: Config.load(), petrification_timer_ref: nil]
 
   defp via_pid(module, init_args, config) do
     {:via, __MODULE__.Registry, {module, init_args, config}}
@@ -111,8 +111,9 @@ defmodule PersistentGenServer do
       :not_found ->
         with {:ok, internal_state} <- module.init(init_args),
              state = %__MODULE__{module: module, init_args: init_args, internal_state: internal_state, config: config},
-             :ok <- persist!(state) do
-          PersistentGenServer.Registry.register_name({module, init_args, config}, self())
+             :ok <- persist!(state),
+             :yes = PersistentGenServer.Registry.register_name({module, init_args, config}, self()),
+             state = reset_petrification_timeout(state) do
           {:ok, state}
         end
       {:error, reason} ->
@@ -174,6 +175,10 @@ defmodule PersistentGenServer do
   end
 
   @impl true
+  def handle_info(:"PersistentGenServer.petrification", state = %__MODULE__{module: module, internal_state: internal_state}) do
+    IO.puts("Petrifying!")
+    {:stop, {:shutdown, :"PersistentGenServer.petrification"}, state}
+  end
   def handle_info(msg, state = %__MODULE__{module: module, internal_state: internal_state}) do
     case module.handle_info(msg, internal_state) do
       {:noreply, new_state} ->
@@ -190,26 +195,45 @@ defmodule PersistentGenServer do
 
   defp persist!(state = %__MODULE__{}) do
     IO.inspect state, label: "persisting state"
+    state = put_in(state.petrification_timer_ref, nil) # We do not want to persist this field ever, since it contains a Timer reference.
 
     # TODO nicer error handling?
     :ok = state.config.storage_implementation.store({state.module, state.init_args}, state)
+    :ok
   end
 
   defp update_and_persist(old_state, new_internal_state) do
     new_state = put_in old_state.internal_state, new_internal_state
     persist!(new_state)
 
-    new_state
+    reset_petrification_timeout(new_state)
+  end
+
+  defp reset_petrification_timeout(state) do
+    if state.config.petrification_timeout == 0 do
+      state
+    else
+      case state.petrification_timer_ref do
+        ref when is_reference(ref) -> Process.cancel_timer(ref)
+        nil -> :ok
+      end
+      new_timer_ref = Process.send_after(self(), :"PersistentGenServer.petrification", state.config.petrification_timeout)
+      put_in(state.petrification_timer_ref, new_timer_ref)
+    end
   end
 
   @impl true
   def terminate(reason, state) do
     IO.inspect({reason, state}, label: "Terminate called!")
     case reason do
+      {:shutdown, :"PersistentGenServer.petrification"} ->
+        :ok
       :normal ->
+        state.module.terminate(reason, state.internal_state)
         wipe!(state)
         :ok
-      _ ->
+      other ->
+        state.module.terminate(reason, state.internal_state)
         :ok
     end
   end
