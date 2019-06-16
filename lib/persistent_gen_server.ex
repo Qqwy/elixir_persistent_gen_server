ae435ceb42c2df7a945eab9d308a81e42701b29e
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-06-16 23:31:49 +0200

    mix format

--- lib/persistent_gen_server.ex
+++ lib/persistent_gen_server.ex
@@ -1,5 +1,6 @@
 defmodule PersistentGenServer do
   use GenServer
+
   @moduledoc """
   PersistentGenServer makes your GenServers Persistent!
 
@@ -25,6 +26,7 @@ defmodule PersistentGenServer do
 
   defmodule Config do
     require Specify
+
     Specify.defconfig do
       @doc """
       The actual storage implementation that will be used
@@ -33,7 +35,7 @@ defmodule PersistentGenServer do
 
       Should be a module name implementing the `PersistentGenServer.Storage` behaviour.
       """
-      field :storage_implementation, :atom, default: PersistentGenServer.Storage.ETS
+      field(:storage_implementation, :atom, default: PersistentGenServer.Storage.ETS)
 
       @doc """
       A timeout before the GenServer auto-unloads ('petrifies') itself.
@@ -41,13 +43,13 @@ defmodule PersistentGenServer do
       '0' means: indefinite.
       TODO maybe change to`:infinity`?
       """
-      field :petrification_timeout, :integer, default: 1
+      field(:petrification_timeout, :integer, default: 1)
 
       @doc """
       The atom name (or module name) of the Dynamic Supervisor
       that should supervise this GenServer.
       """
-      field :dynamic_supervisor, :atom, default: PersistentGenServer.GlobalSupervisor
+      field(:dynamic_supervisor, :atom, default: PersistentGenServer.GlobalSupervisor)
 
       @doc """
       The process registry that is used to keep track of which
@@ -57,17 +59,23 @@ defmodule PersistentGenServer do
       which works well, except in Special Circumstances.
       (for instance: on a distributed system this might need more thought).
       """
-      field :subregistry, :atom, default: Registry
+      field(:subregistry, :atom, default: Registry)
 
       @doc """
       These options are used whenever the server is (re)started.
       See `GenServer.start_link/3`'s "Options" section for the allowed values.
       """
-      field :gen_server_options, {:list, :term}, default: []
+      field(:gen_server_options, {:list, :term}, default: [])
     end
   end
 
-  defstruct [:module, :init_args, :internal_state, config: Config.load(), petrification_timer_ref: nil]
+  defstruct [
+    :module,
+    :init_args,
+    :internal_state,
+    config: Config.load(),
+    petrification_timer_ref: nil
+  ]
 
   defp via_pid(module, init_args, config) do
     {:via, __MODULE__.Registry, {module, init_args, config}}
@@ -92,17 +100,25 @@ defmodule PersistentGenServer do
     config = parse_config(gen_server_options)
 
     # NOTE: WE piggyback the call to the module's init in the call to our init, in the call to `GenServer.start_llink` in the child spec expected for DynamicSupervisor.start_child
-    with {:ok, res} <- DynamicSupervisor.start_child(config.dynamic_supervisor, %{id: __MODULE__, start: {GenServer, :start_link, [__MODULE__, {module, init_args, config}, gen_server_options]}}) do
+    with {:ok, res} <-
+           DynamicSupervisor.start_child(config.dynamic_supervisor, %{
+             id: __MODULE__,
+             start:
+               {GenServer, :start_link,
+                [__MODULE__, {module, init_args, config}, gen_server_options]}
+           }) do
       IO.inspect(res, label: "DynamicSupervisor result")
       {:ok, via_pid(module, init_args, config)}
     end
   end
 
   defp parse_config(gen_server_options) do
-    {passed_in_config, gen_server_options} = pop_in(gen_server_options[:persistent_gen_server_options])
+    {passed_in_config, gen_server_options} =
+      pop_in(gen_server_options[:persistent_gen_server_options])
+
     (passed_in_config || [])
     |> put_in([:gen_server_options], gen_server_options)
-    |> Config.load_explicit
+    |> Config.load_explicit()
   end
 
   # Fills state from cache if avaiable.
@@ -113,46 +129,54 @@ defmodule PersistentGenServer do
       {:ok, state} ->
         PersistentGenServer.Registry.register_name({module, init_args, state.config}, self())
         {:ok, state}
+
       :not_found ->
         init_new(module, init_args, config)
+
       {:error, reason} ->
         {:error, reason}
     end
   end
 
   defp init_new(module, init_args, config) do
-  with  {:ok, internal_state} <- module.init(init_args),
+    with {:ok, internal_state} <- module.init(init_args),
          state = %__MODULE__{
-       module: module,
-       init_args: init_args,
-       internal_state: internal_state,
-       config: config},
+           module: module,
+           init_args: init_args,
+           internal_state: internal_state,
+           config: config
+         },
          :ok <- persist!(state),
          :yes = PersistentGenServer.Registry.register_name({module, init_args, config}, self()),
          state = reset_petrification_timeout(state) do
-    {:ok, state}
-       end
+      {:ok, state}
+    end
   end
 
   # Simple Pass-through to wrapped module
   @impl true
   def handle_call(call, from, state = %__MODULE__{module: module, internal_state: internal_state}) do
     IO.inspect({call, from, state}, label: "handle_call")
+
     case module.handle_call(call, from, internal_state) do
       {:reply, reply, new_state} ->
         {:reply, reply, update_and_persist(state, new_state)}
+
       {:reply, reply, new_state, extra} ->
         {:reply, reply, update_and_persist(state, new_state), extra}
 
       {:noreply, reply, new_state} ->
         {:noreply, reply, update_and_persist(state, new_state)}
+
       {:noreply, reply, new_state, extra} ->
         {:noreply, reply, update_and_persist(state, new_state), extra}
 
       {:stop, reason, reply, new_state} ->
         {:stop, reason, reply, update_and_persist(state, new_state)}
+
       {:stop, reason, new_state} ->
         {:stop, reason, update_and_persist(state, new_state)}
+
       other ->
         other
     end
@@ -164,11 +188,13 @@ defmodule PersistentGenServer do
     case module.handle_cast(call, internal_state) do
       {:noreply, new_state} ->
         {:noreply, update_and_persist(state, new_state)}
+
       {:noreply, new_state, extra} ->
         {:noreply, update_and_persist(state, new_state), extra}
 
       {:stop, reason, new_state} ->
         {:stop, reason, update_and_persist(state, new_state)}
+
       other ->
         other
     end
@@ -176,15 +202,20 @@ defmodule PersistentGenServer do
 
   # Simple Pass-through to wrapped module
   @impl true
-  def handle_continue(continue, state = %__MODULE__{module: module, internal_state: internal_state}) do
+  def handle_continue(
+        continue,
+        state = %__MODULE__{module: module, internal_state: internal_state}
+      ) do
     case module.handle_continue(continue, internal_state) do
       {:noreply, new_state} ->
         {:noreply, update_and_persist(state, new_state)}
+
       {:noreply, new_state, extra} ->
         {:noreply, update_and_persist(state, new_state), extra}
 
       {:stop, reason, new_state} ->
         {:stop, reason, update_and_persist(state, new_state)}
+
       other ->
         other
     end
@@ -193,27 +224,34 @@ defmodule PersistentGenServer do
   # In the special petrification case, shuts down this process for petrification.
   # otherwise, passes through to wrapped module.
   @impl true
-  def handle_info(:"PersistentGenServer.petrification", state = %__MODULE__{module: module, internal_state: internal_state}) do
+  def handle_info(
+        :"PersistentGenServer.petrification",
+        state = %__MODULE__{module: module, internal_state: internal_state}
+      ) do
     IO.puts("Petrifying!")
     {:stop, {:shutdown, :"PersistentGenServer.petrification"}, state}
   end
+
   def handle_info(msg, state = %__MODULE__{module: module, internal_state: internal_state}) do
     case module.handle_info(msg, internal_state) do
       {:noreply, new_state} ->
         {:noreply, update_and_persist(state, new_state)}
+
       {:noreply, new_state, extra} ->
         {:noreply, update_and_persist(state, new_state), extra}
 
       {:stop, reason, new_state} ->
         {:stop, reason, update_and_persist(state, new_state)}
+
       other ->
         other
     end
   end
 
   defp persist!(state = %__MODULE__{}) do
-    IO.inspect state, label: "persisting state"
-    state = put_in(state.petrification_timer_ref, nil) # We do not want to persist this field ever, since it contains a Timer reference.
+    IO.inspect(state, label: "persisting state")
+    # We do not want to persist this field ever, since it contains a Timer reference.
+    state = put_in(state.petrification_timer_ref, nil)
 
     # TODO nicer error handling?
     :ok = state.config.storage_implementation.store({state.module, state.init_args}, state)
@@ -221,7 +259,7 @@ defmodule PersistentGenServer do
   end
 
   defp update_and_persist(old_state, new_internal_state) do
-    new_state = put_in old_state.internal_state, new_internal_state
+    new_state = put_in(old_state.internal_state, new_internal_state)
     persist!(new_state)
 
     reset_petrification_timeout(new_state)
@@ -235,7 +273,14 @@ defmodule PersistentGenServer do
         ref when is_reference(ref) -> Process.cancel_timer(ref)
         nil -> :ok
       end
-      new_timer_ref = Process.send_after(self(), :"PersistentGenServer.petrification", state.config.petrification_timeout)
+
+      new_timer_ref =
+        Process.send_after(
+          self(),
+          :"PersistentGenServer.petrification",
+          state.config.petrification_timeout
+        )
+
       put_in(state.petrification_timer_ref, new_timer_ref)
     end
   end
@@ -249,13 +294,16 @@ defmodule PersistentGenServer do
   @impl true
   def terminate(reason, state) do
     IO.inspect({reason, state}, label: "Terminate called!")
+
     case reason do
       {:shutdown, :"PersistentGenServer.petrification"} ->
         :ok
+
       :normal ->
         state.module.terminate(reason, state.internal_state)
         wipe!(state)
         :ok
+
       other ->
         state.module.terminate(reason, state.internal_state)
         :ok
